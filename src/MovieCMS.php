052dd87955665a25938ea18f396895e52670f9fc
Jaime Tijerina <ti.erina@gmail.com>
2019-05-02 21:08:29 -0400

    added a third party router

--- src/MovieCMS.php
+++ src/MovieCMS.php
@@ -1,10 +1,19 @@
 <?php
 namespace MovieCMS;
-
+use \Bramus\Router\Router;
 class MovieCMS
 {
-	function __construct() {
-	 	echo "Hello World :)";
+	function __construct(Router $router) {
+
+
+
+		// 404 handler
+	 	$router->set404(function() {
+		    header('HTTP/1.1 404 Not Found');
+		});
+
+	 	//https://github.com/bramus/router
+	 	$router->run();
 	}
 }
 ?>
