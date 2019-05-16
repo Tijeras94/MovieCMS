94c55616e9249b05f53849fd71b38c1ecf246389
Jaime Tijerina <ti.erina@gmail.com>
2019-05-16 18:32:16 -0500

    Added 404 json response

--- src/MovieCMS.php
+++ src/MovieCMS.php
@@ -19,10 +19,36 @@ class MovieCMS
 		// 404 handler
 	 	$router->set404(function() {
 		    header('HTTP/1.1 404 Not Found');
+		    echo MovieCMS::json_response("Invalid Entry Point :(");
 		});
 
 	 	//https://github.com/bramus/router
 	 	$router->run();
+	}
+
+	static function json_response($message = null, $code = 200)
+	{
+	    // clear the old headers
+	    header_remove();
+	    // set the actual code
+	    http_response_code($code);
+	    // set the header to make sure cache is forced
+	    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+	    // treat this as json
+	    header('Content-Type: application/json');
+	    $status = array(
+	        200 => '200 OK',
+	        400 => '400 Bad Request',
+	        422 => 'Unprocessable Entity',
+	        500 => '500 Internal Server Error'
+	        );
+	    // ok, validation error, or failure
+	    header('Status: '.$status[$code]);
+	    // return the encoded json
+	    return json_encode(array(
+	        'status' => $code < 300, // success or not?
+	        'message' => $message
+	        ));
 	}
 }
 ?>
